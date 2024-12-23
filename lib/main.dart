866724ee38dde8df2bb7a626d33ee92d4021e351
Motaz <motazyusuf@gmail.com>
2024-12-23 19:49:17 +0200

    innit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/configurations/app_router.dart';
+import 'core/configurations/pages_routes.dart';
+import 'core/theme/application_theme_manager.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -9,6 +14,15 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ApplicationThemeManager.theme,
+        initialRoute: PagesRoutes.onBoardingView,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
+    );
   }
 }
