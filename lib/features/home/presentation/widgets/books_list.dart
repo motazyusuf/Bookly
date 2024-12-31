f6e8734bd6db3f5a773b31f7ead5fe50941290fa
Motaz <motazyusuf@gmail.com>
2024-12-31 09:23:00 +0200

    -Books list change into carousel slide

--- lib/features/home/presentation/widgets/books_list.dart
+++ lib/features/home/presentation/widgets/books_list.dart
@@ -1,5 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'books_list_item.dart';
 
@@ -8,17 +8,33 @@ class BooksList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 8.w),
-      height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.w),
-          child: BooksListItem(),
-        ),
-        itemCount: 4,
-      ),
-    );
+    // return Container(
+    //   margin: EdgeInsets.only(left: 8.w),
+    //   height: 200,
+    //   child: ListView.builder(
+    //     scrollDirection: Axis.horizontal,
+    //     itemBuilder: (context, index) => Padding(
+    //       padding: EdgeInsets.symmetric(horizontal: 8.w),
+    //       child: BooksListItem(),
+    //     ),
+    //     itemCount: 4,
+    //   ),
+    // );
+    return CarouselSlider.builder(
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) =>
+            BooksListItem(),
+        options: CarouselOptions(
+            enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+            padEnds: false,
+            height: 200,
+            disableCenter: false,
+            viewportFraction: 0.39,
+            initialPage: 0,
+            enableInfiniteScroll: false,
+            enlargeCenterPage: true,
+            enlargeFactor: 0.15,
+            scrollDirection: Axis.horizontal,
+            autoPlayCurve: Easing.legacy));
   }
 }
